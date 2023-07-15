ce3d445692c25f98642b26d1376f19147e3239dc
dzastsed <63074962+dzastsed@users.noreply.github.com>
2023-07-15 16:09:52 +0300

    Add files via upload

--- pizzabot.py
+++ pizzabot.py
@@ -3,11 +3,16 @@ from discord.ext import commands
 from pymysql.converters import escape_string
 from discord.ext.commands import bot_has_permissions
 from dotenv import load_dotenv
-from discord_slash import SlashCommand
+from datetime import datetime
+#from discord_slash import SlashCommand
+
+#now = datetime.now()
+#dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-guild_ids = [340493057390804993]
+guild_ids =
+whitelist_ids = []
 
 def escape(s):
     if isinstance(s, str):
@@ -15,15 +20,15 @@ def escape(s):
     return escape(str(s))
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-slash = SlashCommand(bot, sync_commands=True)
+#slash = SlashCommand(bot, sync_commands=True)
 
 async def initSql():
     global pool
     pool = await aiomysql.create_pool(
             host='127.0.0.1',
             port=3306,
-            user='user',
-            password='1223344556',
+            user='',
+            password='',
             db='pizzabot',
             charset='utf8mb4',
             loop=asyncio.get_event_loop()
@@ -38,12 +43,14 @@ initDone = False
 
 @bot.event
 async def on_ready():
+    now = datetime.now()
+    dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")
     global initDone
-    print('Logged in as {}#{}'.format(bot.user.name, bot.user.discriminator))
+    print(dtstring, 'Logged in as {}#{}'.format(bot.user.name, bot.user.discriminator))
     await initSql()
     #await migrate() #uncomment if you want to migrate from v1, it took around 15 hours for 200k messages to migrate on my machine
     initDone = True
-    print('Loaded')
+    print(dtstring, 'Loaded')
 
 async def mysqlQuery(q):
     global pool
@@ -56,9 +63,15 @@ async def mysqlQuery(q):
                 return await conn.commit()
 
 async def logMessage(channel, text):
+    now = datetime.now()
+    dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")
     if text == '':
         return
-    print('Logging a message in channel {}...'.format(channel))
+    
+    if text.startswith("--"):
+        return
+        
+    print(dtstring, 'Logging a message in channel {}...'.format(channel))
     last = await mysqlQuery('''SELECT content FROM messages
                                 WHERE channel='{}'
                                 ORDER BY id desc
@@ -93,63 +106,75 @@ async def logMessage(channel, text):
                             escape(arr[i].lower())
                         )
                     )
-    print('Logged!!')
+    print(dtstring, 'Logged!!')
 
 @bot.event
 async def on_message(msg):
+   # now = datetime.now()
+   # dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")
     global initDone
-    if msg.author.bot and not msg.author.id == 849689355773018192 or not initDone:
+    if msg.author.bot or not initDone:
         return
     #return
 
     message = msg.clean_content
 
+    if message.startswith("--"):
+        return
+
     await logMessage(msg.channel.id, message)
 
-    if bot_has_permissions(send_messages=True):
-        async with msg.channel.typing():
-            msgArray = message.split()
-
-            try:
-                startWord = await mysqlQuery('''SELECT content FROM messages WHERE previousWord = {0}
-                                UNION ALL SELECT content FROM messages WHERE previousWord = {0} AND secondLastWord = {1}
-                                UNION ALL SELECT content FROM messages WHERE previousWord = {0} AND secondLastWord = {1}
-                                ORDER BY rand() LIMIT 1'''
-                                .format(
-                                    escape(msgArray[-1].lower()),
-                                    escape(msgArray[-2].lower() if len(msgArray) > 1 else ' ') #word cant be a space because we split by space
+    if isWhitelisted(msg):     
+        if bot_has_permissions(send_messages=True):
+            async with msg.channel.typing():
+                msgArray = message.split()
+        
+                try:
+                    startWord = await mysqlQuery('''SELECT content FROM messages WHERE previousWord = {0}
+                                    UNION ALL SELECT content FROM messages WHERE previousWord = {0} AND secondLastWord = {1}
+                                    UNION ALL SELECT content FROM messages WHERE previousWord = {0} AND secondLastWord = {1}
+                                    ORDER BY rand() LIMIT 1'''
+                                    .format(
+                                        escape(msgArray[-1].lower()),
+                                        escape(msgArray[-2].lower() if len(msgArray) > 1 else ' ') #word cant be a space because we split by space
+                                    )
                                 )
-                            )
-                if len(startWord) < 1:
-                    print('start word not found')
-                    raise Exception('couldnt find a word')
-                print('Start word found')
-                content, = startWord[0]
-                startWord = content.split(' ')[0]
-                msgLen = random.randint(1, 10)
-                message = [startWord]
-                word = startWord
-                for i in range(msgLen):
-                    word = await mysqlQuery('''SELECT word FROM words
-                                    WHERE previousWord={}
-                                    ORDER BY rand()
-                                    LIMIT 1'''
-                                .format(
-                                    escape(word.lower())
+                    if len(startWord) < 1:
+                        now = datetime.now()
+                        dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")                                                
+                        print(dtstring, 'start word not found')
+                        raise Exception(dtstring, 'couldnt find a word')
+                    now = datetime.now()
+                    dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")
+                    print(dtstring, 'Start word found')
+                    content, = startWord[0]
+                    startWord = content.split(' ')[0]
+                    msgLen = random.randint(1, 10)
+                    message = [startWord]
+                    word = startWord
+                    for i in range(msgLen):
+                        word = await mysqlQuery('''SELECT word FROM words
+                                        WHERE previousWord={}
+                                        ORDER BY rand()
+                                        LIMIT 1'''
+                                    .format(
+                                        escape(word.lower())
+                                    )
                                 )
-                            )
-                    if len(word) == 0:
-                        break
-                    word, = word[0]
-                    message.append(word)
-                    print('next word appended')
-                message = ' '.join(message)
-                if message != '':
-                    await msg.channel.send(message)
-                    return
-            except:
-                pass
-            #await msg.channel.send('no u')
+                        if len(word) == 0:
+                            break
+                        word, = word[0]
+                        message.append(word)
+                        now = datetime.now()
+                        dtstring = now.strftime("[%Y-%m-%d %H:%M:%S]")                        
+                        print(dtstring, 'next word appended')
+                    message = ' '.join(message)
+                    if message != '':
+                        await msg.channel.send(message)
+                        return
+                except:
+                    pass
+                #await msg.channel.send('no u')
 
 async def migrate():
     files = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f)) and f.endswith('.txt')]
@@ -162,17 +187,18 @@ async def migrate():
                 except:
                     pass
 
-@slash.slash(name="restart", guild_ids=guild_ids, description="restart pizzasad if it doesnt work")
-async def restart(ctx):
-  embed=discord.Embed(title=":white_check_mark:",desc="Successfully Restarted")
-  await ctx.send("yo momma gay")
-  os.system("clear")
-  os.execv(sys.executable, ['python'] + sys.argv)
-  await ctx.send("succesfully restarted")
+#@slash.slash(name="restart", guild_ids=guild_ids, description="restart pizzasad if it doesnt work")
+#async def restart(ctx):
+#  embed=discord.Embed(title=":white_check_mark:",desc="Successfully Restarted")
+#  await ctx.send("yo momma gay")
+#  os.system("clear")
+#  os.execv(sys.executable, ['python'] + sys.argv)
+#  await ctx.send("succesfully restarted")
 
-@bot.event
-async def on_member_update(before, after):
-    if before.id == 499660709458870293:
-        print("{} has gone {}.".format(after.name,after.status))
-
+def isWhitelisted(msg):
+    for id in whitelist_ids:
+        if id == msg.channel.id:
+            return True     
+    return False
+      
 bot.run(TOKEN)
